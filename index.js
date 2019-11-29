defc8e450c282251838e2aef634ea0eb8d197978
ArisatoX <patricksungkharisma27@gmail.com>
2019-11-30 01:58:10 +0700

    Implementasi Texture (belum foto sendiri)

--- index.js
+++ index.js
@@ -11,24 +11,24 @@
   var vertices = [];
 
   var cubePoints = [
-    [ -0.9, -0.9,  0.9 ],
-    [ -0.9,  0.9,  0.9 ],
-    [  0.9,  0.9,  0.9 ],
-    [  0.9, -0.9,  0.9 ],
-    [ -0.9, -0.9, -0.9 ],
-    [ -0.9,  0.9, -0.9 ],
-    [  0.9,  0.9, -0.9 ],
-    [  0.9, -0.9, -0.9 ]
+    [ -0.8, -0.8,  0.8 ],
+    [ -0.8,  0.8,  0.8 ],
+    [  0.8,  0.8,  0.8 ],
+    [  0.8, -0.8,  0.8 ],
+    [ -0.8, -0.8, -0.8 ],
+    [ -0.8,  0.8, -0.8 ],
+    [  0.8,  0.8, -0.8 ],
+    [  0.8, -0.8, -0.8 ]
   ];
 
   var cubeColors = [
     [],
-    [100/255, 100/255, 100/255], 
-    [100/255, 100/255, 100/255], 
-    [100/255, 100/255, 100/255], 
-    [100/255, 100/255, 100/255], 
-    [100/255, 100/255, 100/255], 
-    [100/255, 100/255, 100/255], 
+    [1.0, 1.0, 1.0], // merah
+    [0.0, 1.0, 0.0], // hijau
+    [0.0, 0.0, 1.0], // biru
+    [1.0, 0.0, 0.0], // putih
+    [1.0, 0.5, 0.0], // oranye
+    [1.0, 1.0, 0.0], // kuning
     []
   ];
 
@@ -60,41 +60,58 @@
   //Quad
   function quad(a, b, c, d) {
     var indices = [a, b, c, a, c, d];
-
     for (var i=0; i < indices.length; i++) {
-      
       for (var j=0; j < 3; j++) {
         vertices.push(cubePoints[indices[i]][j]);
       }
-
       for (var j=0; j < 3; j++) {
         vertices.push(cubeColors[a][j]);
       }
-
       for (var j=0; j < 3; j++) {
         vertices.push(cubeNormals[a][j]);
       }
-    }
-  }
-
-  function quadline(a, b, c, d) {
-    var indices = [a, b, c, d, a];
-
-    for (var i=0; i < indices.length; i++) {
+      switch (indices[i]) {
+        case a:
+          vertices.push(0.0);
+          vertices.push(0.0);
+          break;
+        case b:
+          vertices.push(0.0);
+          vertices.push(1.0);
+          break;
+        case c:
+          vertices.push(1.0);
+          vertices.push(1.0);
+          break;
+        case d:
+          vertices.push(1.0);
+          vertices.push(0.0);
+          break;
       
-      for (var j=0; j < 3; j++) {
-        vertices.push(cubePoints[indices[i]][j]);
+        default:
+          break;
       }
-
-      for (var j=0; j < 3; j++) {
-        vertices.push(cubeColors[a][j]);
-      }
-
-      // for (var j=0; j < 3; j++) {
-      //   vertices.push(cubeNormals[a][j]);
-      // }
     }
   }
+
+  // function quadline(a, b, c, d) {
+  //   var indices = [a, b, c, d, a];
+
+  //   for (var i=0; i < indices.length; i++) {
+      
+  //     for (var j=0; j < 3; j++) {
+  //       vertices.push(cubePoints[indices[i]][j]);
+  //     }
+
+  //     for (var j=0; j < 3; j++) {
+  //       vertices.push(cubeColors[a][j]);
+  //     }
+
+  //     for (var j=0; j < 3; j++) {
+  //       vertices.push(cubeNormals[a][j]);
+  //     }
+  //   }
+  // }
 
   // GL Size
   function initGlSize() {
@@ -132,51 +149,57 @@
   //Init Buffer Cube
   function initBuffers(gl, vertices) {
 
-     // Membuat vertex buffer object (CPU Memory <==> GPU Memory)
-     var vertexBufferObject = gl.createBuffer();
-     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
-     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
- 
-     // Membuat sambungan untuk attribute
-     var vPosition = gl.getAttribLocation(program, 'vPosition');
-     var vColor = gl.getAttribLocation(program, 'vColor');
-    //  var vNormal = gl.getAttribLocation(program, 'vNormal');
+    // Membuat vertex buffer object (CPU Memory <==> GPU Memory)
+    var vertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-     ntotal = vertices.length / 6;
+    // Membuat sambungan untuk attribute
+    //Cube
+    var vPosition = gl.getAttribLocation(program, 'vPosition');
+    var vNormal = gl.getAttribLocation(program, 'vNormal');
+    var vTexCoord = gl.getAttribLocation(program, 'vTexCoord');
+      
+    ntotal = vertices.length / 6;
+  
+    //Attrib Pointer
+    //Cube
+    gl.vertexAttribPointer(
+      vPosition,    // variabel yang memegang posisi attribute di shader
+      3,            // jumlah elemen per atribut
+      gl.FLOAT,     // tipe data atribut
+      gl.FALSE, 
+      11 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap verteks (overall) 
+      0                                   // offset dari posisi elemen di array
+    );
+
+    //Cube
+    gl.vertexAttribPointer(
+      vNormal, 
+      3, 
+      gl.FLOAT, 
+      gl.FALSE,
+      11 * Float32Array.BYTES_PER_ELEMENT, 
+      6 * Float32Array.BYTES_PER_ELEMENT
+    );
     
-     //Attrib Pointer
-     gl.vertexAttribPointer(
-       vPosition,    // variabel yang memegang posisi attribute di shader
-       3,            // jumlah elemen per atribut
-       gl.FLOAT,     // tipe data atribut
-       gl.FALSE, 
-       6 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap verteks (overall) 
-       0                                   // offset dari posisi elemen di array
-     );
- 
-     gl.vertexAttribPointer(
-       vColor, 
-       3, 
-       gl.FLOAT, 
-       gl.FALSE,
-       6 * Float32Array.BYTES_PER_ELEMENT, 
-       3 * Float32Array.BYTES_PER_ELEMENT
-       );
- 
-    //  gl.vertexAttribPointer(
-    //    vNormal, 
-    //    3, 
-    //    gl.FLOAT, gl.FALSE,
-    //    9 * Float32Array.BYTES_PER_ELEMENT, 
-    //    6 * Float32Array.BYTES_PER_ELEMENT
-    //    );  
- 
-     //EnableVertexAttrib
-     gl.enableVertexAttribArray(vPosition);
-     gl.enableVertexAttribArray(vColor);
-    //  gl.enableVertexAttribArray(vNormal);
+    gl.vertexAttribPointer(
+      vTexCoord, 
+      2, 
+      gl.FLOAT, 
+      gl.FALSE,
+      11 * Float32Array.BYTES_PER_ELEMENT, 
+      9 * Float32Array.BYTES_PER_ELEMENT
+    );
 
-     return ntotal;
+    
+
+    //Cube
+    gl.enableVertexAttribArray(vPosition);
+    gl.enableVertexAttribArray(vNormal);
+    gl.enableVertexAttribArray(vTexCoord);
+
+    return ntotal;
   }
 
   //Init Buffer P
@@ -209,7 +232,7 @@
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     var vPositionP = gl.getAttribLocation(program, 'vPositionP');
-    var vColorP = gl.getAttribLocation(program, 'vColorP');
+    var vColor = gl.getAttribLocation(program, 'vColor');
 
     gl.vertexAttribPointer(
       vPositionP,  // variabel yang memegang posisi attribute di shader
@@ -220,11 +243,17 @@
       0                                   // offset dari posisi elemen di array
     );
 
-    gl.vertexAttribPointer( vColorP, 3, gl.FLOAT, gl.FALSE, 
-      6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+    gl.vertexAttribPointer(
+      vColor, 
+      3, 
+      gl.FLOAT, 
+      gl.FALSE,
+      6 * Float32Array.BYTES_PER_ELEMENT, 
+      3 * Float32Array.BYTES_PER_ELEMENT
+    );
 
     gl.enableVertexAttribArray(vPositionP);
-    gl.enableVertexAttribArray(vColorP);
+    gl.enableVertexAttribArray(vColor);
 
     return ntotal;
   }
@@ -259,12 +288,12 @@
     );
     gl.uniformMatrix4fv(pmLoc, false, pm);
 
-    //Uniform untuk pencahayaan
+    // Uniform untuk pencahayaan
     var dcLoc = gl.getUniformLocation(program, 'diffuseColor');
-    var dc = glMatrix.vec3.fromValues(1.0, 1.0, 1.0); //rgb
+    var dc = glMatrix.vec3.fromValues(1.0, 1.0, 1.0);  // rgb
     gl.uniform3fv(dcLoc, dc);
-    var ddLoc = gl.getUniformLocation(program, 'diffuseDirection');
-    var dd = glMatrix.vec3.fromValues(0.5, 3.0, 4.0);  //xyz
+    var ddLoc = gl.getUniformLocation(program, 'diffusePosition');
+    var dd = glMatrix.vec3.fromValues(1., 2., 1.7);  // xyz
     gl.uniform3fv(ddLoc, dd);
     var acLoc = gl.getUniformLocation(program, 'ambientColor');
     var ac = glMatrix.vec3.fromValues(0.2, 0.2, 0.2);
@@ -272,6 +301,33 @@
 
     //Uniform untuk modelMatrix vektor normal
     nmLoc = gl.getUniformLocation(program, 'normalMatrix');
+
+    // Create a texture.
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Fill the texture with a 1x1 blue pixel.
+    gl.texImage2D(
+      gl.TEXTURE_2D, 
+      0, 
+      gl.RGBA, 
+      1, 
+      1, 
+      0, 
+      gl.RGBA, 
+      gl.UNSIGNED_BYTE,
+      new Uint8Array([0, 0, 255, 255])
+    );
+
+    // Asynchronously load an image
+    var image = new Image();
+    image.src = "images/tes.bmp";
+    image.addEventListener('load', function() {
+      // Now that the image has loaded make copy it to the texture.
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+      gl.generateMipmap(gl.TEXTURE_2D);
+    });
 
     //Set scale refleksi
     scaleXUniformLocation = gl.getUniformLocation(program, 'scaleX');
@@ -311,7 +367,10 @@
     //Clear Color
     gl.clearColor(255/255, 221/255, 209/255, 1.0);
     gl.enable(gl.DEPTH_TEST);
+
     render();
+
+
   }
 
   function render() {
@@ -339,29 +398,29 @@
     flag = 0.0;
     gl.uniform1f(flagUniformLocation, flag);
     // gl.drawArrays(gl.TRIANGLES, 0, 36);
-    gl.drawArrays(gl.LINE_STRIP, 0, n1);
+    gl.drawArrays(gl.TRIANGLES, 0, n1);
 
     //P
     
     //Animasi Rotasi
     if (scaleX >= 1.0) span = -1.0;
     else if (scaleX <= -1.0) span = 1.0;
-    scaleX += 0.041 * span;
+    scaleX += 0.01 * span;
     gl.uniform1f(scaleXUniformLocation, scaleX);
 
     //Animasi Translasi
-    if (translateX >= 0.9 - Math.abs(scaleX * 0.07)) animationX = -1.0;
-    else if (translateX <= -0.9 + Math.abs(scaleX * 0.07)) animationX = 1.0;
-    translateX += 0.015 * animationX;
+    if (translateX >= 0.8 - Math.abs(scaleX * 0.07)) animationX = -1.0;
+    else if (translateX <= -0.8 + Math.abs(scaleX * 0.07)) animationX = 1.0;
+    translateX += 0.01 * animationX;
     gl.uniform1f(translateXUniformLocation, translateX);
 
-    if (translateY >= 0.9 - 0.4) animationY = -1.0;
-    else if (translateY <= -0.9 + 0.1) animationY = 1.0;
+    if (translateY >= 0.8 - 0.4) animationY = -1.0;
+    else if (translateY <= -0.8 + 0.1) animationY = 1.0;
     translateY += 0.011 * animationY;
     gl.uniform1f(translateYUniformLocation, translateY);
 
-    if (translateZ >= 0.9) animationZ = -1.0;
-    else if (translateZ <= -0.9) animationZ = 1.0;
+    if (translateZ >= 0.8) animationZ = -1.0;
+    else if (translateZ <= -0.8) animationZ = 1.0;
     translateZ += 0.013 * animationZ;
     gl.uniform1f(translateZUniformLocation, translateZ);
 
@@ -393,18 +452,18 @@
     gl.useProgram(program);
 
     // quad(1, 0, 3, 2);
-    // quad(2, 3, 7, 6);
-    // quad(3, 0, 4, 7);
-    // quad(4, 5, 6, 7);
-    // quad(5, 4, 0, 1);
-    // quad(6, 5, 1, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+    quad(6, 5, 1, 2);
 
-    quadline(1, 0, 3, 2); // Depan
-    quadline(2, 3, 7, 6); // Kanan
-    quadline(6, 5, 1, 2); // Atas
-    quadline(5, 4, 0, 1); // Kiri
-    quadline(4, 7, 3, 0); // Bawah
-    quadline(4, 5, 6, 7); // Belakang
+    // quadline(1, 0, 3, 2); // Depan
+    // quadline(2, 3, 7, 6); // Kanan
+    // quadline(6, 5, 1, 2); // Atas
+    // quadline(5, 4, 0, 1); // Kiri
+    // quadline(4, 7, 3, 0); // Bawah
+    // quadline(4, 5, 6, 7); // Belakang
 
     n1 = initBuffers(gl,vertices);
     n2 = initBuffers2(gl, vertices5, vertices6, 0.15, -0.07, 0.25);
--- vertex.glsl
+++ vertex.glsl
@@ -2,24 +2,23 @@ precision mediump float;
 
 //Cube
 attribute vec3 vPosition;
-attribute vec3 vColor;
+attribute vec2 vTexCoord;
 attribute vec3 vNormal;
 
-varying vec3 fColor;
+varying vec3 fNormal;
+varying vec3 fPosition;
+varying vec2 fTexCoord;
 
 uniform float theta;
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;  //Posisi kamera
 uniform mat4 modelMatrix;
-
-uniform vec3 diffuseColor;
-uniform vec3 diffuseDirection;
-uniform mat3 normalMatrix;  //Berperan sebagai modelMatrix vector normal
-uniform vec3 ambientColor;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat3 normalMatrix;  // Berperan sebagai modelMatrix-nya vektor normal
 
 //P
 attribute vec3 vPositionP;
-attribute vec3 vColorP;
+varying vec3 fColor;
+attribute vec3 vColor;
 uniform float scaleX, scaleY, translateX, translateY, translateZ;
 
 //Switch
@@ -45,15 +44,23 @@ void main()
 
   if(flag == 0.0)
   {
-    fColor = vColor;
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vPosition, 1.0);
 
+    // Transfer koordinat tekstur ke fragment shader
+    fTexCoord = vTexCoord;
+
+    // Transfer vektor normal (yang telah ditransformasi) ke fragment shader
+    fNormal = normalize(normalMatrix * vNormal);
+
+    // Transfer posisi verteks
+    fPosition = vec3(modelMatrix * vec4(vPosition, 1.0));
 
   }
   else if(flag == 1.0)
   {
-    fColor = vColorP;
+    fColor = vColor;
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * mTransformP * mScale * vec4(vPositionP, 1.0);
+
   }
   
 }
