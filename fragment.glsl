512697cfece22558060b163c5422b8a3090fd6f5
ArisatoX <patricksungkharisma27@gmail.com>
2019-12-08 21:32:13 +0700

    update fragment + specular

--- fragment.glsl
+++ fragment.glsl
@@ -11,6 +11,8 @@ uniform vec3 ambientColor;
 
 varying vec3 fColor;
 uniform float flag;
+float specularLight;
+float specular;
 
 void main() {
 
@@ -19,12 +21,24 @@ void main() {
     vec3 diffuseDirection = normalize(diffusePosition - fPosition);
     float normalDotLight = max(dot(fNormal, diffuseDirection), 0.0);
 
+    specularLight = 100.0;
+    specular = 0.0;
+
+    if (normalDotLight > 0.0) 
+    {
+      vec3 vv = vec3(0, 0, 1.0);
+      vec3 rv = reflect(-diffuseDirection, fNormal);
+
+      float specularFactor = max (dot(rv, vv), 0.0);
+      specular = pow(specularFactor, specularLight);
+    }
+
     vec4 textureColor = texture2D(sampler0, fTexCoord);
 
     vec3 diffuse = diffuseColor * textureColor.rgb * normalDotLight;
     vec3 ambient = ambientColor * textureColor.rgb;
 
-    gl_FragColor = vec4(diffuse + ambient, 1.0);
+    gl_FragColor = vec4(diffuse + ambient + specular, 1.0);
   }
   
   else if(flag == 1.0)
